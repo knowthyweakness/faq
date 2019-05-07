b3c144dbf085e8c8dd66b2ce7c777cd48c674933
knowthyweakness <tanvir97>
2019-05-07 12:58:47 -0400

    Added Answer migrate, model, and made answer seeder and added tests for user profile(cleanup) and answer test

--- database/migrations/2019_05_07_152723_create_answers_table.php
+++ database/migrations/2019_05_07_152723_create_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
+            $table->integer('question_id');
             $table->longText('body');
             $table->timestamps();
         });
@@ -27,6 +28,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('answers');
     }
 }
